86b49be1d31672fe8afcab58e16b21f92f874a04
mkallmayer <mel.kallmayer@gmail.com>
2019-04-29 19:20:03 +0200

    semi-pretty images

--- randombg.py
+++ randombg.py
@@ -2,29 +2,36 @@
 from PIL import Image as image
 import numpy as np
 from random import randrange as rr
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-WIDTH = 100
-HEIGHT = 100
+import scipy.misc
+
+WIDTH = 300
+HEIGHT = 300
 EPSILON = 0.001
 
 # select a few random pixels as spots
-spots = np.array([[rr(0,WIDTH), rr(0, HEIGHT)] for i in range(0,rr(3,5))], dtype=np.float64)
+spots = np.array([[rr(0,WIDTH), rr(0, HEIGHT)] for i in range(0,rr(3,6))], dtype=np.float64)
 # assign random rgb values to spots
 spot_cols = np.array([[rr(0,255) for i in range(3)] for j in range(len(spots))], dtype=np.float64)
 
-def metric(xpos, ypos):
+def metric1(xpos, ypos):
     return abs(xpos[0] - ypos[0]) + abs(xpos[1] - ypos[1])
 
-grid = np.zeros((WIDTH, HEIGHT, 3), dtype=np.float64)
+def metric2(xpos, ypos):
+    return np.sqrt((xpos[0] - ypos[0])**2 + (xpos[1] - ypos[1])**2)
+
+grid = np.zeros((WIDTH, HEIGHT, 3), dtype=np.uint8)
 
 for y in range(HEIGHT):
     for x in range(WIDTH):
         dists = np.zeros((len(spots)), dtype=np.uint8)
         for s in range(len(spots)):
-            dists[s] = metric(spots[s], grid[y, x])
-        totdists = dists.sum()
-        for s in range(len(spots)):
-            grid[x, y] += spot_cols[s] * (dists[s]/totdists)
+            dists[s] = metric1(spots[s], [x, y])
+        index = np.argmin(dists)
+        grid[x, y] = spot_cols[index]
 
-im = image.fromarray(grid, 'RGB')
-im.save('my.png')
+grid = grid
+im = image.fromarray(grid)
+im.save("out.png")
